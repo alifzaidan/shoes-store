d4291a0e5efdfefc73a4a9c9a259c40f2950a2e1
alifzaidan <zaidanalif22710@gmail.com>
2025-04-25 11:17:41 +0700

    minggu 3 jumat

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -11,7 +11,7 @@ import { useInitials } from '@/hooks/use-initials';
 import { cn } from '@/lib/utils';
 import { type BreadcrumbItem, type NavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-react';
+import { Home, Menu, ShoppingCart } from 'lucide-react';
 import AppLogo from './app-logo';
 import AppLogoIcon from './app-logo-icon';
 
@@ -19,20 +19,15 @@ const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
         href: '/dashboard',
-        icon: LayoutGrid,
+        icon: Home,
     },
 ];
 
 const rightNavItems: NavItem[] = [
     {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits',
-        icon: BookOpen,
+        title: 'Cart',
+        href: '/',
+        icon: ShoppingCart,
     },
 ];
 
@@ -126,9 +121,6 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
 
                     <div className="ml-auto flex items-center space-x-2">
                         <div className="relative flex items-center space-x-1">
-                            <Button variant="ghost" size="icon" className="group h-9 w-9 cursor-pointer">
-                                <Search className="!size-5 opacity-80 group-hover:opacity-100" />
-                            </Button>
                             <div className="hidden lg:flex">
                                 {rightNavItems.map((item) => (
                                     <TooltipProvider key={item.title} delayDuration={0}>
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,4 +1,5 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
@@ -15,19 +16,48 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                <div className="border-sidebar-border/70 dark:border-sidebar-border relative flex min-h-[50vh] flex-1 flex-col justify-center overflow-hidden rounded-xl border bg-[url(/images/hero.jpeg)] bg-cover pl-4 md:pl-24">
+                    <h1 className="text-2xl font-semibold">Langkah Pasti, Gaya Tanpa Batas</h1>
+                    <p className="mb-4 text-sm text-gray-500 md:w-2xl">
+                        Temukan koleksi sepatu terbaik untuk setiap langkahmu. Dari sepatu dewasa hingga anak-anak, kami punya semua yang kamu
+                        butuhkan.
+                    </p>
+                    <Button className="w-fit">Belanja Sekarang</Button>
+                </div>
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border bg-[url(/images/man-shoes.png)] bg-cover bg-center p-4">
+                        <Badge variant="secondary">Sepatu Pria</Badge>
                     </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border bg-[url(/images/woman-shoes.png)] bg-cover bg-bottom p-4">
+                        <Badge variant="secondary">Sepatu Wanita</Badge>
                     </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border bg-[url(/images/kids-shoes.png)] bg-cover bg-center p-4">
+                        <Badge variant="secondary">Sepatu Anak-anak</Badge>
                     </div>
                 </div>
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                <h1 className="text-2xl font-semibold">Produk Kita</h1>
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border overflow-hidden rounded-xl border p-4">
+                        <img src="/images/nike-shox-tl.jpg" alt="Adidas Samba" className="rounded-lg" />
+                        <h2 className="mt-2 text-lg font-semibold">Nike Shox TL</h2>
+                        <p className="text-sm text-gray-500">Sepatu Pria</p>
+                        <p className="text-lg font-semibold">Rp 2.600.000</p>
+                        <Button className="mt-2 w-full">Lihat Produk</Button>
+                    </div>
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border overflow-hidden rounded-xl border p-4">
+                        <img src="/images/nike-air-force-1.jpg" alt="Adidas Samba" className="rounded-lg" />
+                        <h2 className="mt-2 text-lg font-semibold">Nike Air Force 1</h2>
+                        <p className="text-sm text-gray-500">Sepatu Wanita</p>
+                        <p className="text-lg font-semibold">Rp 1.500.000</p>
+                        <Button className="mt-2 w-full">Lihat Produk</Button>
+                    </div>
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border overflow-hidden rounded-xl border p-4">
+                        <img src="/images/nike-air-max-systm.jpg" alt="Adidas Samba" className="rounded-lg" />
+                        <h2 className="mt-2 text-lg font-semibold">Nike Air Max SYSTM</h2>
+                        <p className="text-sm text-gray-500">Sepatu Anak-anak</p>
+                        <p className="text-lg font-semibold">Rp 800.000</p>
+                        <Button className="mt-2 w-full">Lihat Produk</Button>
+                    </div>
                 </div>
             </div>
         </AppLayout>
